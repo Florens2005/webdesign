cb5e692eb1dcd43b84edbce0ca812e554a89e491
Florens2005 <kuifie2005@gmail.com>
2023-10-12 13:45:31 +0200

    about toegevoegd

--- searchbar.js
+++ searchbar.js
@@ -14,10 +14,11 @@ searchInput.addEventListener("input", e => {
     })
 })
 
-const header = document.getElementById('searchbar');
+const header = document.getElementById('header');
 const images = ['https://www.starwarsnewsnet.com/wp-content/uploads/2021/11/FEAXvmoXEAoIWfE.jpg'
 , 'https://pbs.twimg.com/media/E0KcbNcVEAIbSDT.jpg:large'
-, 'https://pbs.twimg.com/media/EiV8RnBUMAAhZ0t?format=jpg&name=large']; // Array of image filenames
+, 'https://pbs.twimg.com/media/EiV8RnBUMAAhZ0t?format=jpg&name=large'
+,"https://media.comicbook.com/2015/11/banner-kylo-158585.jpg"]; // Array of image filenames
 let index = 0;
 
 header.style.transition = 'background-image 1s linear'; // Add a transition property to the header element
